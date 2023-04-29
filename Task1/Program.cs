ff753ec83cef447346a62ee4ece7e41d1ed4afbe
sahib.novruzov <sahib.novruzov@MP-OSD-N03>
2023-04-29 08:55:47 +0400

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double l, r;
+        r = 2;
+        l = 2 * Math.PI * r;
+        Console.WriteLine(l);
+        Console.ReadLine();
+    }
+}
